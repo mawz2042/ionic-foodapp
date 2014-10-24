68945c302981b623b6042a05a1e8bfd7687c9d53
CyberKronos <alex44nguyen@gmail.com>
2014-10-23 18:21:10 -0700

    added more functionality: - go to phrase is now on card - general card clean up - resized images and added placeholder - can now filter distance and price

--- www/php/foursquare.php
+++ www/php/foursquare.php
@@ -11,17 +11,19 @@ $V_TOKEN = "20141010";
 $DEFAULT_TERM = 'food';
 $DEFAULT_LAT = "43.6000";
 $DEFAULT_LONG = "-79.6500";
-$DEFAULT_RADIUS = 2000; // in m
+$DEFAULT_RADIUS = 500; // in m
 $SEARCH_LIMIT = 50;
+$DEFAULT_PRICE = "1,2,3,4";
 
-function explore($term, $lat, $long, $radius) {    
+function explore($term, $lat, $long, $radius, $price) {    
     $newTerm = $term ?: $GLOBALS['DEFAULT_TERM'];
     $newLat = $lat ?: $GLOBALS['DEFAULT_LAT'];
     $newLong = $long ?: $GLOBALS['DEFAULT_LONG'];
     $newRadius = $radius ?: $GLOBALS['DEFAULT_RADIUS'];
     $newLimit = $GLOBALS['SEARCH_LIMIT'];
+    $newPrice = $price ?: $GLOBALS['DEFAULT_PRICE'];
 
-    $foursquare_url = "https://api.foursquare.com/v2/venues/explore?ll=".$newLat.",".$newLong."&section=".$newTerm."&radius=".$newRadius."&limit=".$newLimit."&venuePhotos=1&oauth_token=".$GLOBALS['OAUTH_TOKEN']."&v=".$GLOBALS['V_TOKEN'];
+    $foursquare_url = "https://api.foursquare.com/v2/venues/explore?ll=".$newLat.",".$newLong."&section=".$newTerm."&radius=".$newRadius."&price=".$newPrice."&limit=".$newLimit."&venuePhotos=1&oauth_token=".$GLOBALS['OAUTH_TOKEN']."&v=".$GLOBALS['V_TOKEN'];
     $foursquare_call = file_get_contents($foursquare_url);
     // $response = json_decode($foursquare_call, TRUE);
     print $foursquare_call;
@@ -31,16 +33,16 @@ function explore($term, $lat, $long, $radius) {
  * User input is handled here 
  */
 $longopts  = array(
-    "term::",
-    "radius::"
+    "term::"
 );
 
 $options = getopt("", $longopts);
 $term = $options['term'] ?: '';
-$radius = $options['radius'] ?: '';
     
 $lat = $request->latitude;
 $long = $request->longitude;
+$radius = $request->distance;
+$price = $request->price;
 
-return explore($term, $lat, $long, $radius);
+return explore($term, $lat, $long, $radius, $price);
 ?>
